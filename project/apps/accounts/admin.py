f365d7ae5fb009996c3e8d85fb490e81365b173b
Oliver Hung Buo Tso <olivertsor@gmail.com>
2018-07-25 19:46:29 -0300

    Better admin for user model.

--- project/apps/accounts/admin.py
+++ project/apps/accounts/admin.py
@@ -3,15 +3,59 @@ from django.contrib import admin
 from .models import User
 
 
+class AddedProgressesFilter(admin.SimpleListFilter):
+    title = "Added progresses"
+    parameter_name = 'added_progresses'
+
+    def lookups(self, request, model_admin):
+        return [('yes', "Yes"), ('no', "No")]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+
+        ids = [user.id for user in queryset if user.added_progresses_count]
+        if self.value() == 'yes':
+            return queryset.filter(id__in=ids)
+        return queryset.exclude(id__in=ids)
+
+
+class FollowedProgressesFilter(admin.SimpleListFilter):
+    title = "Followed progresses"
+    parameter_name = 'followed_progresses'
+
+    def lookups(self, request, model_admin):
+        return [('yes', "Yes"), ('no', "No")]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+
+        ids = [user.id for user in queryset if user.followed_progresses_count]
+        if self.value() == 'yes':
+            return queryset.filter(id__in=ids)
+        return queryset.exclude(id__in=ids)
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'max_followed_progresses',
+    list_display = ('email', 'added_progresses_count', 'followed_progresses_count',
+                    'max_followed_progresses', 'is_active', 'is_staff', 'is_superuser',
                     'created')
-    list_filter = ('is_active', 'is_staff', 'is_superuser')
-    search_fields = ('email',)
-    fields = ('id', 'email', 'is_active', 'is_staff', 'is_superuser', 'max_followed_progresses',
-              'created')
-    readonly_fields = ('id', 'is_superuser', 'created')
+    list_filter = (AddedProgressesFilter, FollowedProgressesFilter, 'is_active', 'is_staff',
+                   'is_superuser')
+    search_fields = ('id', 'email',)
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'created', 'email', 'is_active', 'is_staff', 'is_superuser')
+        }),
+        ("Progresses", {
+            'fields': ('added_progresses_count', 'followed_progresses_count',
+                       'max_followed_progresses')
+        }),
+    )
+    readonly_fields = ('id', 'is_superuser', 'added_progresses_count', 'followed_progresses_count',
+                       'created')
 
     def has_add_permission(self, request):
         return False
