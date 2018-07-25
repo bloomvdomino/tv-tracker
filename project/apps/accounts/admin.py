946acc418da30b3925ef11e14e9ae67d262ae731
Oliver Hung Buo Tso <olivertsor@gmail.com>
2018-07-24 21:04:00 -0300

    Adjustments.

--- project/apps/accounts/admin.py
+++ project/apps/accounts/admin.py
@@ -6,12 +6,12 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'max_followed_progresses',
-                    'created', 'last_login')
+                    'created')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     search_fields = ('email',)
     fields = ('id', 'email', 'is_active', 'is_staff', 'is_superuser', 'max_followed_progresses',
-              'created', 'last_login')
-    readonly_fields = ('id', 'is_superuser', 'created', 'last_login')
+              'created')
+    readonly_fields = ('id', 'is_superuser', 'created')
 
     def has_add_permission(self, request):
         return False
