a7a1bd38bb8834198209fbf7048637ce01baa719
Oliver Hung Buo Tso <olivertsor@gmail.com>
2018-08-21 22:26:04 -0300

    Use SimpleRouter.

--- project/apps/tmdb/urls.py
+++ project/apps/tmdb/urls.py
@@ -1,11 +1,11 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from .apps import TMDbConfig
 from .views import ProgressViewSet
 
 app_name = TMDbConfig.label
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'progresses', ProgressViewSet, base_name='progress')
 
 urlpatterns = router.urls
