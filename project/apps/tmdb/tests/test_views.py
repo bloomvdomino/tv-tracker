8ba18b0c3e3a6399742aaa8bc17a52732efaeb40
Oliver Hung Buo Tso <olivertsor@gmail.com>
2018-09-15 12:07:15 -0300

    Adjust tmdb urls.

--- project/apps/tmdb/tests/test_views.py
+++ project/apps/tmdb/tests/test_views.py
@@ -30,18 +30,89 @@ class ProgressViewSetTests(APITestCase):
         self.assertEqual(self.viewset.lookup_field, 'show_id')
 
 
-class ProgressViewSetRetrieveTests(APITestCase):
+class ProgressViewSetCreateTests(APITestCase):
     def setUp(self):
-        self.url = '/tmdb/progresses/'
-        user_1 = User.objects.create_user('u1@test.com', 'foo123')
-        user_2 = User.objects.create_user('u2@test.com', 'foo123')
-        self.client.force_authenticate(user=user_1)
-        self.progress_1 = Progress.objects.create(user=user_1, show_id=1, show_name='Vikings')
-        self.progress_2 = Progress.objects.create(user=user_1, show_id=2, show_name='Shooter')
-        Progress.objects.create(user=user_2, show_id=1, show_name='Vikings')
+        self.url = '/tmdb/progress/'
+        self.user_1 = User.objects.create_user('u1@test.com', 'foo123')
+        self.user_2 = User.objects.create_user('u2@test.com', 'foo123')
+        self.client.force_authenticate(user=self.user_1)
+        self.data = {
+            'show_id': 1,
+            'show_name': 'Vikings',
+            'show_status': Progress.RETURNING
+        }
+
+    def test_201(self):
+        response = self.client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Progress.objects.count(), 1)
+        self.assertEqual(Progress.objects.filter(user=self.user_1, show_id=1).count(), 1)
+
+
+class ProgressViewSetUpdateTests(APITestCase):
+    def setUp(self):
+        self.url = '/tmdb/progress/{}/'
+        self.user_1 = User.objects.create_user('u1@test.com', 'foo123')
+        self.user_2 = User.objects.create_user('u2@test.com', 'foo123')
+        self.client.force_authenticate(user=self.user_1)
+        self.progress = Progress.objects.create(user=self.user_1, show_id=1, show_name='Vikings')
+        self.data = {
+            'show_status': Progress.ENDED
+        }
 
     def test_200(self):
+        response = self.client.patch(self.url.format(self.progress.show_id), data=self.data)
+        self.assertEqual(response.status_code, 200)
+        self.progress.refresh_from_db()
+        self.assertEqual(self.progress.user, self.user_1)
+        self.assertEqual(self.progress.show_id, 1)
+        self.assertEqual(self.progress.show_name, 'Vikings')
+        self.assertEqual(self.progress.show_status, Progress.ENDED)
+
+
+class ProgressViewSetDestroyTests(APITestCase):
+    def setUp(self):
+        self.url = '/tmdb/progress/{}/'
+        self.user_1 = User.objects.create_user('u1@test.com', 'foo123')
+        self.user_2 = User.objects.create_user('u2@test.com', 'foo123')
+        self.progress_1 = Progress.objects.create(user=self.user_1, show_id=1, show_name='Vikings')
+        self.progress_2 = Progress.objects.create(user=self.user_2, show_id=1, show_name='Vikings')
+
+    def test_204_1(self):
+        self.client.force_authenticate(user=self.user_1)
+        response = self.client.delete(self.url.format(1))
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Progress.objects.filter(user=self.user_1).count(), 0)
+        self.assertEqual(Progress.objects.filter(user=self.user_2).count(), 1)
+
+    def test_204_2(self):
+        self.client.force_authenticate(user=self.user_2)
+        response = self.client.delete(self.url.format(1))
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Progress.objects.filter(user=self.user_1).count(), 1)
+        self.assertEqual(Progress.objects.filter(user=self.user_2).count(), 0)
+
+
+class ProgressViewSetListTests(APITestCase):
+    def setUp(self):
+        self.url = '/tmdb/progress/list/'
+        self.user_1 = User.objects.create_user('u1@test.com', 'foo123')
+        self.user_2 = User.objects.create_user('u2@test.com', 'foo123')
+        Progress.objects.create(user=self.user_1, show_id=1, show_name='Vikings')
+        Progress.objects.create(user=self.user_1, show_id=2, show_name='Shooter')
+        Progress.objects.create(user=self.user_2, show_id=3, show_name='Better Call Saul')
+
+    def test_200_1(self):
+        self.client.force_authenticate(user=self.user_1)
         response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]['show_id'], 2)
         self.assertEqual(response.data[1]['show_id'], 1)
+
+    def test_200_2(self):
+        self.client.force_authenticate(user=self.user_2)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['show_id'], 3)
