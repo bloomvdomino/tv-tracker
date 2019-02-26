6d1e252b4eaa0b8956e436850ea2a4b85566678a
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-02-26 20:47:59 -0300

    Popular shows V2 done.

--- project/apps/tmdb/utils.py
+++ project/apps/tmdb/utils.py
@@ -15,12 +15,30 @@ def make_poster_url(path, width):
     return 'https://image.tmdb.org/t/p/{}{}'.format(widths[width], path)
 
 
+def get_popular_shows(page):
+    """
+    Get popular TV shows.
+    https://developers.themoviedb.org/3/tv/get-popular-tv-shows
+    """
+    url = 'https://api.themoviedb.org/3/tv/popular'
+    params = {
+        'api_key': settings.TMDB_API_KEY,
+        'page': page,
+    }
+    res = requests.get(url, params=params)
+    # TODO: handle status code other than 200.
+    return res.json()['results']
+
+
 def search_by_name(name):
+    """
+    Search for a TV show by name.
+    https://developers.themoviedb.org/3/search/search-tv-shows
+    """
     url = 'https://api.themoviedb.org/3/search/tv'
     params = {
         'api_key': settings.TMDB_API_KEY,
         'query': name,
-        'language': 'en-US',
     }
     res = requests.get(url, params=params)
     # TODO: handle status code other than 200.
--- project/apps/tmdb/views.py
+++ project/apps/tmdb/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import FormView, TemplateView
@@ -6,6 +8,7 @@ from rest_framework import mixins, viewsets
 from .forms import SearchForm
 from .models import Progress
 from .serializers import ProgressSerializer
+from .utils import get_popular_shows
 
 
 class ProgressViewSet(mixins.CreateModelMixin,
@@ -26,6 +29,28 @@ class V2ProgressesView(LoginRequiredMixin, TemplateView):
 
 class V2PopularShowsView(TemplateView):
     template_name = 'tmdb/popular_shows.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+
+        min_page = 1
+        max_page = 1000
+
+        page = int(self.request.GET.get('page', min_page))
+        page = page if page >= min_page else min_page
+        page = page if page <= max_page else max_page
+        kwargs.update(current_page=page, shows=get_popular_shows(page))
+
+        if page - 1 >= min_page:
+            kwargs.update(previous_page_link=self.make_page_link(page - 1))
+
+        if page + 1 <= max_page:
+            kwargs.update(next_page_link=self.make_page_link(page + 1))
+
+        return kwargs
+
+    def make_page_link(self, page):
+        return '{}?{}'.format(self.request.path, urlencode({'page': page}))
 
 
 class V2SearchView(FormView):
