5e018cb4aa2ad992efef4d9d7e6c963ca20c28fb
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-03-01 07:28:58 -0300

    Add filter_seasons and some refactor.

--- project/apps/tmdb/utils.py
+++ project/apps/tmdb/utils.py
@@ -37,43 +37,54 @@ def mark_saved_shows(shows, user):
     return shows
 
 
-def get_show(id):
+def filter_seasons(show):
     """
-    Get a TV show detail by ID.
-    https://developers.themoviedb.org/3/tv/get-tv-details
+    Return only non-special and non-empty seasons of the show.
+
+    TMDB API may also return some especial seasons as the first element in the
+    list. And the last season may be empty.
     """
-    url = 'https://api.themoviedb.org/3/tv/{}'.format(id)
-    params = {'api_key': settings.TMDB_API_KEY}
+    n = len(show['seasons']) - show['number_of_seasons']
+    seasons = show['seasons'][n:]
+    if not seasons[-1]:
+        del seasons[-1]
+    show.update(seasons=seasons)
+    return show
+
+
+def fetch(endpoint, params=None):
+    url = 'https://api.themoviedb.org/3/{}'.format(endpoint)
+    params = params or {}
+    params.update(api_key=settings.TMDB_API_KEY)
     res = requests.get(url, params=params)
     # TODO: handle status code other than 200.
     return res.json()
 
 
+def get_show(id):
+    """
+    Get a TV show detail by ID.
+
+    https://developers.themoviedb.org/3/tv/get-tv-details
+    """
+    return filter_seasons(fetch('tv/{}'.format(id)))
+
+
 def get_popular_shows(page):
     """
     Get popular TV shows by page.
+
     https://developers.themoviedb.org/3/tv/get-popular-tv-shows
     """
-    url = 'https://api.themoviedb.org/3/tv/popular'
-    params = {
-        'api_key': settings.TMDB_API_KEY,
-        'page': page,
-    }
-    res = requests.get(url, params=params)
-    # TODO: handle status code other than 200.
-    return add_detail_links(res.json()['results'])
+    shows = fetch('tv/popular', params={'page': page})['results']
+    return add_detail_links(shows)
 
 
 def search_show(name):
     """
     Search for TV shows by name.
+
     https://developers.themoviedb.org/3/search/search-tv-shows
     """
-    url = 'https://api.themoviedb.org/3/search/tv'
-    params = {
-        'api_key': settings.TMDB_API_KEY,
-        'query': name,
-    }
-    res = requests.get(url, params=params)
-    # TODO: handle status code other than 200.
-    return add_detail_links(res.json()['results'])
+    shows = fetch('search/tv', params={'query': name})['results']
+    return add_detail_links(shows)
