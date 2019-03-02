160f3f25fcca661271a41aa9ab53e88c60583dc6
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-03-02 10:45:28 -0300

    Stop showing unaired episodes.

--- project/apps/tmdb/forms.py
+++ project/apps/tmdb/forms.py
@@ -27,24 +27,12 @@ class ProgressForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.show = kwargs.pop('show')
         super().__init__(*args, **kwargs)
-        self.fields['last_watched'].choices = self.make_episode_choices(self.show['seasons'])
+        self.fields['last_watched'].choices = self.make_episode_choices()
 
     def clean_last_watched(self):
         last_watched = self.cleaned_data['last_watched']
-        last_watched_season, last_watched_episode = map(int, last_watched.split('-'))
-
-        last_aired = self.show.get('last_episode_to_air', {})
-        last_aired_season = last_aired.get('season_number', 0)
-        last_aired_episode = last_aired.get('episode_number', 0)
-
-        if last_watched_season > last_aired_season or last_watched_episode > last_aired_episode:
-            raise forms.ValidationError("This episode is not available, yet.")
-
-        self.cleaned_data.update(
-            current_season=last_watched_season,
-            current_episode=last_watched_episode,
-        )
-
+        season, episode = map(int, last_watched.split('-'))
+        self.cleaned_data.update(current_season=season, current_episode=episode)
         return last_watched
 
     def save(self, commit=True):
@@ -56,17 +44,27 @@ class ProgressForm(forms.ModelForm):
         self.instance.current_episode = self.cleaned_data['current_episode']
         return super().save(commit=commit)
 
-    @staticmethod
-    def make_episode_choices(seasons):
+    def make_episode_choices(self):
         episode_choices = [('0-0', "Not started, yet.")]
-        for season, info in enumerate(seasons, 1):
+        for season, info in enumerate(self.show['seasons'], 1):
             for episode in range(1, info['episode_count'] + 1):
+                if not self.episode_aired(season, episode):
+                    break
                 value = '{}-{}'.format(season, episode)
                 season_label = '0{}'.format(season)[-2:]
                 episode_label = '0{}'.format(episode)[-2:]
                 label = "S{}E{}".format(season_label, episode_label)
                 episode_choices.append((value, label))
         return episode_choices
+
+    def episode_aired(self, season, episode):
+        last_aired = self.show.get('last_episode_to_air', {})
+        last_aired_season = last_aired.get('season_number', 0)
+        last_aired_episode = last_aired.get('episode_number', 0)
+        return not (
+            season > last_aired_season or
+            (season == last_aired_season and episode > last_aired_episode)
+        )
 
 
 class SearchForm(forms.Form):
