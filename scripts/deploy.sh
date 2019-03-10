d1edf29f5a5f3fb7021d14c60153ee6afccaa87c
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-03-10 11:07:15 -0300

    :rocket: Update heroku app name.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@
 
 set -e
 
-heroku_app_name=api-tv-tracker
+heroku_app=my-tv-tracker
 process_type=web
-tag=registry.heroku.com/$heroku_app_name/$process_type
+tag=registry.heroku.com/$heroku_app/$process_type
 
 docker build -f docker/prod.Dockerfile -t $tag .
 docker push $tag
-heroku container:release $process_type
+heroku -a $heroku_app container:release $process_type
 docker rmi $(docker images $tag -q)
 
-heroku run python manage.py migrate
+heroku -a $heroku_app run python manage.py migrate
