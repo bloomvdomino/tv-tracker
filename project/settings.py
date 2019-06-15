77966e9a17b6eb6e1db1872748a4108b7373d721
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-06-15 10:20:19 -0300

    :building_construction: Set database backups folder.

--- project/settings.py
+++ project/settings.py
@@ -79,6 +79,7 @@ DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 if ENV in ["qa", "production"]:
     DBBACKUP_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     DBBACKUP_STORAGE_OPTIONS = {"bucket_name": config("BUCKET_NAME"), "default_acl": None}
+    DBBACKUP_FILENAME_TEMPLATE = config("DATABASE_BACKUP_DIR") + "{datetime}.{extension}"
 else:
     DBBACKUP_STORAGE = "django.core.files.storage.FileSystemStorage"
     DBBACKUP_STORAGE_OPTIONS = {"location": os.path.join(BASE_DIR, "db_backups/")}
