88ae2911c9771cb2c246210d451d3340bda21481
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-07-21 11:07:38 -0300

    Update deploy.sh.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-APP=tv-tracker
-TAG=$DOCKER_USERNAME/$APP
+TAG=$DOCKER_USERNAME/tv-tracker
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -f docker/production/Dockerfile -t $TAG .
+docker build -f docker/prod.Dockerfile -t $TAG .
 docker push $TAG
-
-INSTANCE_ID=$(aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=hobby-infra" "Name=tag:Env,Values=production" \
-    --query "Reservations[0].Instances[0].InstanceId" \
-    --output text)
-
-aws ssm send-command \
-    --instance-ids $INSTANCE_ID \
-    --document-name "AWS-RunShellScript" \
-    --parameters commands=["cd /apps/$APP/ && sh app_init.sh"]
