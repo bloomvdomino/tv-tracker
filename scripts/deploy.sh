939b807be2107de7aa94e12c39338d9f6cb91c41
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-07-28 08:25:21 -0300

    Deploy using travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-TAG=$DOCKER_USERNAME/tv-tracker
+ENV=production
+APP=tv-tracker
+TAG=$DOCKER_USERNAME/$APP
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Build and push Docker image.
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -f docker/prod.Dockerfile -t $TAG .
 docker push $TAG
+
+# Replace ECS task.
+CLUSTER=$(aws ecs list-clusters | grep hobby-infra-$ENV | sed 's/"//g' | awk '{$1=$1};1')
+TASK=$(aws ecs list-tasks \
+    --cluster $CLUSTER \
+    --service-name $APP-$ENV \
+    | grep arn:aws:ecs \
+    | sed 's/"//g' \
+    | awk '{$1=$1};1')
+aws ecs stop-task --cluster $CLUSTER --task $TASK --reason Deploy
