8365336ab0e49c630f42a52d8efc9863ceb2ba5c
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-09-21 09:22:39 -0300

    Adjust linter configs

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,10 +5,10 @@ set -e
 DOCKER_COMPOSE="docker-compose run --no-deps --rm web"
 
 echo "Running isort..."
-$DOCKER_COMPOSE isort -rc project
+$DOCKER_COMPOSE isort -rc .
 
 echo "Running black..."
-$DOCKER_COMPOSE black project
+$DOCKER_COMPOSE black .
 
 echo "Running terraform fmt..."
 $DOCKER_COMPOSE terraform fmt -recursive terraform
