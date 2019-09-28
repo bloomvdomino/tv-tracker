86d82ad527b79265234a1e905bef3997740a4cf4
Oliver Tso <olivertsor@gmail.com>
2019-09-28 08:47:46 -0300

    Remove error reports by email

--- terraform/modules/main/main.tf
+++ terraform/modules/main/main.tf
@@ -2,10 +2,6 @@ data "aws_ssm_parameter" "admin_path" {
   name = "${local.parameter_store_path}/admin_path"
 }
 
-data "aws_ssm_parameter" "admins" {
-  name = "${local.parameter_store_path}/admins"
-}
-
 data "aws_ssm_parameter" "tmdb_api_key" {
   name = "${local.parameter_store_path}/tmdb_api_key"
 }
@@ -30,7 +26,6 @@ resource "heroku_app" "default" {
     SECRET_KEY = random_string.secret_key.result
 
     ADMIN_PATH   = data.aws_ssm_parameter.admin_path.value
-    ADMINS       = data.aws_ssm_parameter.admins.value
     TMDB_API_KEY = data.aws_ssm_parameter.tmdb_api_key.value
   }
 }
