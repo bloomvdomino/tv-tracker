09aedf7131ada142b86c6529c2f658e35ec13bfb
Oliver Tso <olivertsor@gmail.com>
2019-10-06 10:34:17 -0300

    Remove health check url

--- project/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from project.core.apps import CoreConfig
-from project.core.views import HealthCheckView
-
-app_name = CoreConfig.label
-
-urlpatterns = [path("health/", HealthCheckView.as_view(), name="health_check")]
--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,6 @@ admin.site.unregister(Group)
 urlpatterns = [
     path("", include("project.apps.tmdb.urls", namespace="tmdb")),
     path("", include("project.apps.website.urls", namespace="website")),
-    path("", include("project.core.urls", namespace="core")),
     path("accounts/", include("project.apps.accounts.urls", namespace="accounts")),
     path(settings.ADMIN_PATH, admin.site.urls),
 ]
